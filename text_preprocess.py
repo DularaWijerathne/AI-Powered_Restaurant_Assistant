a97bbee1316db6263615bb71b49c7d5b9a33295d
DularaWijerathne <dularawjerathne@gmail.com>
2024-02-08 12:07:55 +0530

    updated text_preprocess.py

---
+++ text_preprocess.py
@@ -0,0 +1,29 @@
+import nltk
+import numpy as np
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+
+# nltk.download('wordnet')
+lemmatizer = WordNetLemmatizer()
+
+
+# split a string into units(words, punctuations, numbers)
+def tokenize(sentence):
+    return word_tokenize(sentence)
+
+
+# generate the root from the words.
+def lemmatize(word):
+    return lemmatizer.lemmatize(word.lower())
+
+
+def bag_of_words(tokenized_sentence, all_words):
+    bag = np.zeros(len(all_words))
+    for (index, word) in enumerate(all_words):
+        if word in tokenized_sentence:
+            bag[index] = 1.0
+
+    return bag
+
+
+
--- train.py
+++ train.py
@@ -1,6 +1,45 @@
+import numpy as np
 import json
+from text_preprocess import tokenize, lemmatize, bag_of_words
 
 with open('Intents/intents.json') as f:
     data = json.load(f)
 
-print(data['intents'])
+tags = []  # list of tags
+tokens = []  # list of all the words, punctuations and numbers
+tags_with_tokens = []
+
+for intent in data['intents']:
+    tag = intent['tag']
+    tags.append(tag)
+
+    for pattern in intent['patterns']:
+        token = tokenize(pattern)
+        tokens.extend(token)
+
+        tags_with_tokens.append((tag, token))
+
+ignore_characters = ['?', '!', '.', ',', '-']
+lemmatized_tokens = [lemmatize(i) for i in tokens if i not in ignore_characters]
+unique_tags = sorted(set(tags))  # remove duplicates from tags
+unique_tokens = sorted(set(lemmatized_tokens))  # remove duplicates from tokens
+
+X_train = []
+y_train = []
+
+for (tag, pattern_sentence) in tags_with_tokens:
+
+    label = tags.index(tag)
+    y_train.append(label)
+
+    lemmatized_pattern_sentence = [lemmatize(i) for i in pattern_sentence]
+    X_train.append(bag_of_words(lemmatized_pattern_sentence, unique_tokens))
+
+X_train = np.array(X_train)
+y_train = np.array(y_train)
+
+
+print(tags_with_tokens)
+print(unique_tokens)
+print(X_train)
+print(y_train)
