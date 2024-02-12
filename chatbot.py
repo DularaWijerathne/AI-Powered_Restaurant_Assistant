f0767a9fa2fa8e9bf5d405cd11f2aa0185fbcffb
DularaWijerathne <dularawjerathne@gmail.com>
2024-02-12 12:11:12 +0530

    exported tags and tokens

---
+++ chatbot.py
@@ -0,0 +1,14 @@
+import pickle
+import random
+import json
+import numpy as np
+import pandas as pd
+
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+
+from tensorflow.keras.models import load_model
+
+gg = bag
+print(gg)
+
--- train.py
+++ train.py
@@ -1,3 +1,4 @@
+import pickle
 import numpy as np
 import json
 from text_preprocessor import tokenize, lemmatize, bag_of_words
@@ -15,7 +16,7 @@ ignore_characters = ['?', '!', '.', ',', '-']
 
 for intent in data['intents']:
     tag = intent['tag']
-    tags.append(tag)
+    tags.append(tag.lower())
 
     for pattern in intent['patterns']:
         token = tokenize(pattern)
@@ -26,6 +27,9 @@ for intent in data['intents']:
 lemmatized_tokens = [lemmatize(i) for i in tokens if i not in ignore_characters]
 unique_tags = sorted(set(tags))  # remove duplicates from tags
 unique_tokens = sorted(set(lemmatized_tokens))  # remove duplicates from tokens
+
+pickle.dump(unique_tags, open('tags', 'wb'))
+pickle.dump(unique_tokens, open('tokens', 'wb'))
 
 training_data = []
 
